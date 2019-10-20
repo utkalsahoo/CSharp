7813f2056c44a9c46d349fc90682216b7216f563
utkalsahoo <utkalsahoo987@gmail.com>
2019-10-20 19:45:26 +0530

    Hello World

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Hello
+{
+   class HelloWorld
+   {
+       static void Main(String args[])
+       {
+           System.Console.WriteLine("Hello World!");
+       }
+   } 
+}

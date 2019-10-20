29c2a314502ec93a116ee99e5f5af4d09f4409fa
utkalsahoo <utkalsahoo987@gmail.com>
2019-10-20 20:14:19 +0530

    Data Types

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = 10;
+            var Char= 'A';
+            var String = "Utkal";
+            var Float = 1.54f;
+            var Decimal = 1.45d;
+            Console.WriteLine(a);
+            Console.WriteLine(Char);
+            Console.WriteLine(String);
+            Console.WriteLine(Float);
+            Console.WriteLine(Decimal);
+        }
+    }
+}

096a199295af1e8cb27348e698c135d366958f93
utkalsahoo <utkalsahoo987@gmail.com>
2019-11-01 11:14:26 +0530

    Program to find area of a trapezoid

---
+++ Trapezoid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TrapezoidArea
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the value of a:");
+            var a = Convert.T Console.ReadLine();
+            Console.Write("Enter the value of b:");
+            var b = Console.ReadLine();
+            Console.WriteLine("Enter the value of h:");
+            var h = Console.ReadLine();
+
+        }
+        public static double AreaOfTrapezoid(float a,float b,float h)
+        {
+            var Area = (a + b) / 2 * h;
+            return Area;
+        }
+    }
+}

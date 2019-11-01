2c4fee9f2838cab09287554b905419c703bee02d
utkalsahoo <utkalsahoo987@gmail.com>
2019-11-01 11:10:29 +0530

    Program to reverse a string in C#

---
+++ StringReverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class StringReverse
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            var string1= Console.ReadLine();
+            Console.WriteLine(Reverse_String(string1));
+        }
+        public static string Reverse_String(string string1)
+        {
+            var reversedString = "";
+            for (var i = string1.Length - 1; i >= 0; i--)
+                reversedString = reversedString + string1[i];
+            return reversedString;
+        }
+    }
+}

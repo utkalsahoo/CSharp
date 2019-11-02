15d56cade1874145e0c8cd16e1961b98fce391c7
utkalsahoo <utkalsahoo987@gmail.com>
2019-11-02 10:42:52 +0530

    Program to calculate the number of zeroes in the factorial of a given number

---
+++ ZeroInFactorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ZeroInFactorial
+{
+    class ZeroInFactorial
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Please Enter a Number: ");
+            var number = Int32.Parse(Console.ReadLine());
+            int zero = 0;
+            var factorial=Factorial(number);
+            var noOfZero = CountZero(factorial, ref zero);
+            Console.Write("The Factorial is {0} and it has ", factorial);
+            Console.WriteLine("{0} zeroes.", zero);
+        }
+        public static long Factorial(int number)
+        {
+            long factorial = number;
+            for (int i = number - 1; i > 0; i--)
+                factorial *= i;
+            return factorial;
+        }
+        public static int CountZero(long factorial,ref int zero)
+        {
+            do
+            {
+                factorial /= 10;
+                zero++;
+            } while (factorial % 10 == 0);
+            return zero;
+        }
+    }
+}

b7fe1c1d06b9fc66a616f600b12e35d438760838
utkalsahoo <utkalsahoo987@gmail.com>
2019-11-02 10:39:37 +0530

    Compare teo real number with accuracy at least 0.000001

---
+++ CompareTwoRealNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CompareRealNumber
+{
+    class CompareTwoRealNumber
+    {
+        static void Main(string[] args)
+        {
+            var number1 = 11.778788779f;
+            var number2 = 11.778788778f;
+            Console.WriteLine(Accuracy(number1,number2));
+        }
+        public static bool Accuracy(float number1, float number2)
+        {
+            bool equal = Math.Abs(number1 - number2) < 0.000001;
+            return equal;
+        }
+    }
+}

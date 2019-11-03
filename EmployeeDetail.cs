0cc3f7364b6158797934217f44d73f3688b2bf09
utkalsahoo <utkalsahoo987@gmail.com>
2019-11-03 10:50:52 +0530

    Program to assigne various data types to mentain the records of employee of a company

---
+++ EmployeeDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace EmployeeDetails
+{
+    class EmployeeDetail
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Your Age: ");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Please enter your gender(m 0r f): ");
+            char gender = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Please Enter your unique Employee Number: ");
+            int employeeId = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
